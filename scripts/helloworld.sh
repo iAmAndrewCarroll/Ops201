8996d5e93eaa786ed1bd05faaff98f49cc0f3fb8
iAmAndrewCarroll <athomascarroll@gmail.com>
2023-10-24 11:06:26 -0700

    Ops Challenge 2 Complete

---
+++ scripts/helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script Name:                  Hello World
+# Author:                       Andrew Carroll
+# Date of latest revision:      10/24/2023
+# Purpose:                      Ops201 Challenge 02
+
+# Declaration of variables
+my_variable="Hello World!"
+
+# Declaration of functions
+
+
+# Main
+
+echo $my_variable
+
+# End
