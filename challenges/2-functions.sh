3af118196e2e710693e310408802fb48f24a8166
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-10-25 10:14:59 -0700

    login history script

---
+++ challenges/2-functions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script:                     Login History  
+# Author:                     Andrew Carroll  
+# Date of latest revision:    10/25/2024  
+# Purpose:                    Ops 201 Challenge 02
+
+# Declaration of functions
+
+# Basic function
+login_history () {
+  last | head -3 # last five logins
+  echo this is the login history 
+}
+
+# Main
+login_history
+login_history
+login_history
+
+
+# End
