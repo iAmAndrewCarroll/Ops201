3e621a10e39339151fced67007e135fb70ba094d
iAmAndrewCarroll <athomascarroll@gmail.com>
2023-11-07 16:54:09 -0800

    challenge 12

--- challenges/12-ipget.ps1
+++ challenges/12-ipget.ps1
@@ -1,18 +1,14 @@
-# Function to get IPv4 address
-function Get-IPv4Address {
-    $ipConfigOutput = ipconfig /all
-    $ipAddress = $ipConfigOutput | Select-String -Pattern 'IPv4 Address' | ForEach-Object { $_ -replace '\D+(\d+\.\d+\.\d+\.\d+).*', '$1' }
-    return $ipAddress
-}
+# Specify the save path
+$savePath = "C:\Users\admin\Documents\network_report.txt"
 
-# Create network_report.txt file
-ipconfig /all > network_report.txt
+# Create network_report.txt file by running ipconfig /all and saving the output
+ipconfig /all > $savePath
 
-# Get IPv4 address using the function
-$ipv4Address = Get-IPv4Address
+# Search for IPv4 address in network_report.txt using Select-String
+$ipv4Address = Get-Content $savePath | Select-String -Pattern 'IPv4 Address' | ForEach-Object { $_ -replace '\D+(\d+\.\d+\.\d+\.\d+).*', '$1' }
 
 # Display the IPv4 address
 Write-Host "IPv4 Address: $ipv4Address"
 
-# Remove the network_report.txt file
-Remove-Item -Path "network_report.txt" -Force
+# Remove the temporary file
+Remove-Item -Path $savePath -Force
